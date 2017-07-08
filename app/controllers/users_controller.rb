6a517dc33dc12c50ad57ea41f6a492edf5389fe5
Koji Del Mundo <gian.delmundo@obf.ateneo.edu>
2017-07-08 07:58:50 +0000

    fixed Edit Cashier

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,12 @@ class UsersController < ApplicationController
     
     def update
         @user = User.find(params[:id])
-
+        
+        if params[:user][:password].blank?
+          params[:user].delete(:password)
+          params[:user].delete(:password_confirmation)
+        end
+        
         if @user.update(user_params)
           redirect_to @user
         else
