667405c3948728fcf4ad6ca7d82d33e53ada41e9
Koji Del Mundo <gian.delmundo@obf.ateneo.edu>
2017-07-15 04:03:37 +0000

    fixed devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_cache_buster
-  
+
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    devise_for :admins
-    devise_for :users, path_prefix: 'd'
+    devise_for :admins, path: '', path_names: { sign_in: "", sign_out: "" }
+    devise_for :users, path: '', path_names: { sign_in: "", sign_out: "" }
     resources :users do
         post :deactivate, on: :member
         post :activate, on: :member
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
     end
     
     resources :orders
-    resources :messages
     
     get '/report', to: "reports#index"
     post '/report', to: "reports#create"
