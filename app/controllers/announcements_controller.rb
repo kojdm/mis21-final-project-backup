95ccaa2104f91cc380f3589ff300e7a23032658f
Koji Del Mundo <gian.delmundo@obf.ateneo.edu>
2017-07-15 07:48:51 +0000

    initial commit for backup

---
+++ app/controllers/announcements_controller.rb
@@ -0,0 +1,5 @@
+class AnnouncementsController < ApplicationController
+
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-    devise_for :admins, path: '', path_names: { sign_in: "", sign_out: "" }
-    devise_for :users, path: '', path_names: { sign_in: "", sign_out: "" }
+    
+    devise_for :users, path: '', path_names: {sign_in: "", sign_out: ""}
+    devise_for :admins, path: '', path_names: {sign_in: "welcome/admin_login", sign_out: ""}
+    
     resources :users do
         post :deactivate, on: :member
         post :activate, on: :member
@@ -12,10 +14,12 @@ Rails.application.routes.draw do
     end
     
     resources :orders
+    resources :announcements
     
     get '/report', to: "reports#index"
     post '/report', to: "reports#create"
     
-    
-    root 'welcome#index'
+    devise_scope :user do
+        root to: 'devise/sessions#new'
+    end
 end
